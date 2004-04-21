30395545a5d424a2bf605f34aca445bdf5743f21
nas <nas@67cd4824-ecf2-0310-a53c-f99d37e220e1>
2004-04-21 19:29:19 +0000

    *** empty log message ***
    git-svn-id: file:///srv/svn.allmed/trunk/ezpos@100 67cd4824-ecf2-0310-a53c-f99d37e220e1

---
+++ pos_settings.rb
@@ -0,0 +1,64 @@
+
+require 'gconf2'
+require 'customer'
+
+module POSSetting
+    @check_account = @credit_card_account = @cash_account = Customer.find( 'CASH' )
+
+    def POSSetting.init
+	@gConf = GConf::Client.new
+	@tax = @gConf["/apps/ezpos/tax_rate"]
+	@print_header =	@gConf['/apps/ezpos/print_header']
+	@print_header = "" if ! @print_header
+	@tax=0.0 if ! @tax
+	@taxExtemp = false;
+    end
+
+    def POSSetting.tax_rate=( tax )
+	@gConf['/apps/ezpos/tax_rate'] = tax
+	@tax = tax
+	TotalsDisplay.instance.update
+    end
+
+    def POSSetting.tax_rate
+	@tax
+    end
+
+    def POSSetting.tax_exempt=( val )
+	@taxExtemp = val
+    end
+
+    def POSSetting.tax_exempt
+	@taxExtemp
+    end
+
+    def POSSetting.toggle_tax_exempt
+	@taxExtemp = ! @taxExtemp
+    end
+
+
+    def POSSetting.CreditCardAccount
+	@credit_card_account
+    end
+
+    def POSSetting.CashAccount
+	@cash_account
+    end
+
+    def POSSetting.CheckAccount
+	@check_account
+    end
+
+    def POSSetting.customer
+	@cash_account
+    end
+
+    def POSSetting.printHeader
+	@print_header
+    end
+
+    def POSSetting.printHeader=( ph )
+	@gConf['/apps/ezpos/print_header'] = ph
+	@print_header = ph
+    end
+end
