6fa42290f127a899ef9d551a9cbf4cba6177926e
nas <nas@67cd4824-ecf2-0310-a53c-f99d37e220e1>
2004-08-05 21:33:55 +0000

    *** empty log message ***
    git-svn-id: file:///srv/svn.allmed/trunk/ezpos@377 67cd4824-ecf2-0310-a53c-f99d37e220e1

---
+++ drawer-open.c
@@ -0,0 +1,12 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * $Id$ 
+ * Copyright (C) 2002 Nathan Stitt  
+ * See file COPYING for use and distribution permission.
+ */
+
+
+
+int main(){
+	printf("%c",7);
+}
