a88fe4c38f50f72b3488e5ea0f91485569690613
nas <nas@67cd4824-ecf2-0310-a53c-f99d37e220e1>
2005-08-19 14:44:02 +0000

    fixups
    git-svn-id: file:///srv/svn.allmed/trunk/ezpos@1251 67cd4824-ecf2-0310-a53c-f99d37e220e1

--- pos_settings.rb
+++ pos_settings.rb
@@ -4,7 +4,7 @@ require 'singleton'
 module POS
 
 class Setting
-    BAD_CC_SWIPE = ';E/'
+
     include Singleton
 
     attr_accessor :tax_exempt
@@ -21,9 +21,7 @@ class Setting
 	if ! @gConf.dir_exists?( '/apps/ezpos' )
 	    @gConf['/apps/ezpos/pole_welcome_pause'] =  @gConf['/apps/ezpos/pole_thank_you_pause'] = 10
 	    @gConf['/apps/ezpos/pole_welcome_one'] = @gConf['/apps/ezpos/pole_welcome_two'] = @gConf['/apps/ezpos/pole_thank_you_one'] = @gConf['/apps/ezpos/pole_thank_you_two'] = ''
-	    @gConf['/apps/ezpos/proccess_credit_cards']=false
 	end
-
 	@pole_welcome_pause=@gConf['/apps/ezpos/pole_welcome_pause'].to_i
 	@pole_welcome_one=@gConf['/apps/ezpos/pole_welcome_one']
 	@pole_welcome_two=@gConf['/apps/ezpos/pole_welcome_two']
@@ -31,7 +29,7 @@ class Setting
 	@pole_thank_you_pause=@gConf['/apps/ezpos/pole_thank_you_pause'].to_i
 	@pole_thank_you_one=@gConf['/apps/ezpos/pole_thank_you_one']
 	@pole_thank_you_two=@gConf['/apps/ezpos/pole_thank_you_two']
-	@process_cards=@gConf['/apps/ezpos/proccess_credit_cards']
+
 	@drawer_char=7
     end
 
@@ -45,10 +43,7 @@ class Setting
 	@tax_rate = tax
 	TotalsDisplay.instance.update
     end
-    
-    def set_not_tax_exempt
-	@tax_exempt = false
-    end
+
 
     def toggle_tax_exempt
 	@tax_exempt = ! @tax_exempt
