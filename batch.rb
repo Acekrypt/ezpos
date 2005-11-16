24a289af5570aea1bf10b49f30a36850fd662cdf
nas <nas@67cd4824-ecf2-0310-a53c-f99d37e220e1>
2005-11-16 23:47:21 +0000

    added greyed-out  cost
    git-svn-id: file:///srv/svn.allmed/trunk/ezpos@1351 67cd4824-ecf2-0310-a53c-f99d37e220e1

--- batch.rb
+++ batch.rb
@@ -13,14 +13,10 @@ res=NAS::DB.instance.exec( "SELECT transaction_id,amount,occured from payments w
 res.each do | ( trans, amt, dte ) |
 
     res=YourPay.ChargeFace2FaceAuthorization( trans,amt )
-
+    file.puts '-------------------------------------------------'
+    res.each{ |k,v| file.puts "#{k.to_s} => #{v.to_s}" }
     puts sprintf('%-30s%10.2f  %15s   %s',trans, amt.to_f, dte.split('.').first ,res['error'] )
 
-    if res['approved'] != 'APPROVED'
-	res.each{ |k,v| file.puts "#{k.to_s} => #{v.to_s}" }
-    end
-
-    
 end
 puts
 puts
