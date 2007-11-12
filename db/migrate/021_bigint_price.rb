998864a6c5c3cdb4b63c53f1cc87dd671b15fc5c
nas <nas@67cd4824-ecf2-0310-a53c-f99d37e220e1>
2007-11-12 20:59:53 +0000

    git-svn-id: file:///srv/svn.allmed/trunk/ezpos@1917 67cd4824-ecf2-0310-a53c-f99d37e220e1

---
+++ db/migrate/021_bigint_price.rb
@@ -0,0 +1,9 @@
+class BigintPrice < ActiveRecord::Migration
+  def self.up
+      execute "ALTER TABLE skus ALTER COLUMN price type bigint"
+  end
+
+  def self.down
+      execute "ALTER TABLE skus ALTER COLUMN price type int"
+  end
+end
